9ca27e154355d18a0b2bbbe90a7bbcb81c9574e3
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-05-13 21:07:25 +0530

    Update splashscreen.dart

--- splashscreen.dart
+++ splashscreen.dart
@@ -1,6 +1,7 @@
 import 'package:splashscreen/splashscreen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:connectivity/connectivity.dart';
 import 'main.dart';
 void main() => runApp(splashscreen());
 // ignore: camel_case_types
@@ -10,11 +11,32 @@ class splashscreen extends StatefulWidget {
 }
 // ignore: camel_case_types
 class _splashscreenState extends State<splashscreen> {
-  @override
+  final GlobalKey<ScaffoldState> skey = new GlobalKey<ScaffoldState>();
+
+  void isOn()
+  async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile || connectivityResult == ConnectivityResult.wifi)
+    {
+      // ignore: deprecated_member_use
+      skey.currentState.showSnackBar(new SnackBar(content: new Text('You are online',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),
+      ),backgroundColor: Colors.green,
+      )
+      );
+    }
+    else  {
+      // ignore: deprecated_member_use
+      skey.currentState.showSnackBar(new SnackBar(content: new Text('You are offline',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),
+      ),
+        backgroundColor: Colors.red,)
+      );
+    }
+  }
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home : new Scaffold(
+          key : skey,
             body : new SplashScreen(
               seconds: 5,
               navigateAfterSeconds: new Main(),
