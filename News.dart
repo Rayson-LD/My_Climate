cd02dbcc582e69e876d4cb4b53041e6d12bc24db
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-05-22 21:54:57 +0530

    Update News.dart

--- News.dart
+++ News.dart
@@ -1,1 +1,25 @@
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
+class News extends StatefulWidget {
+  @override
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State<News> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+     WebView.platform = SurfaceAndroidWebView();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      minimum: EdgeInsets.zero,
+        child: WebView(
+        javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: 'https://www.accuweather.com/en/weather-news',
+        ));
+  }
+}
 
